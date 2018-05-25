51cbd244c16fdf38be18eae28853c8111dd8ef38
Luca DEttorre <ldettorre21@gmail.com>
2018-05-25 11:28:43 +0000

    basic frame done

--- chatapp.py
+++ chatapp.py
@@ -9,7 +9,15 @@ def get_index(): # this is the function located at the above route/url that gets
     return render_template("index.html")
 
 messages =[]
+banned_words =["fuck"]
 
+
+
+# @app.route('rooms/add')
+# def add_room():
+#     roomname = request.form['roomname']
+#     rooms[roomname] = []
+#     return redirect(....)
 
 
 @app.route("/login")
@@ -23,15 +31,34 @@ def create_user():
 def get_userpage(username):
     return render_template("chat.html",username=username, all_messages=messages)
     
+    
+    
 @app.route("/<username>/new", methods = ["POST"])
 def get_add_message(username):
     text = request.form["message"]
-    
+
+
+
+
+    words = text.split()
+    for i in range(len(words)):
+        for j in range(len(banned_words)):
+            if words[i] == banned_words[j]:
+                words[i] = "*"*len(words[i])
+    text = " ".join(map(str,words))
     message = { 
-        "sender": username,
-        "body": text
-        }
+    "sender": username,
+    "body": text
+    }
+        
+  
+        
+        
     messages.append(message)
     return redirect(username)
 
-app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")),debug = True)
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
