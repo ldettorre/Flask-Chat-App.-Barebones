61735f3184d001d7c58a0fde3473b160ef5c6e3d
Luca DEttorre <ldettorre21@gmail.com>
2018-05-28 11:55:54 +0000

    private message added using @username

--- chatapp.py
+++ chatapp.py
@@ -4,20 +4,15 @@ from flask import Flask, redirect,render_template,request
 app = Flask(__name__)
 
 
-@app.route("/") # at this route in the url return the below
-def get_index(): # this is the function located at the above route/url that gets called when the page is returned.
-    return render_template("index.html")
-
 messages =[]
 banned_words =["cats"]
 
 
 
-# @app.route('rooms/add')
-# def add_room():
-#     roomname = request.form['roomname']
-#     rooms[roomname] = []
-#     return redirect(....)
+@app.route("/") # at this route in the url return the below
+def get_index(): # this is the function located at the above route/url that gets called when the page is returned.
+    return render_template("index.html")
+
 
 
 @app.route("/login")
@@ -26,29 +21,44 @@ def create_user():
     return redirect(username)
     
     
+def can_see_message(message, username):
+    for_everyone = not message["body"].startswith("@")
+    for_this_user = message["body"].startswith("@" + username)
+    user_is_sender = message["sender"] == username
+    
+    return for_everyone or for_this_user or user_is_sender   
+    
+    
+    
     
 @app.route("/<username>")
 def get_userpage(username):
-    return render_template("chat.html", username=username , all_messages=messages)
+    
+     filtered_messages = []
+     for message in messages:
+        if can_see_message(message, username):
+            filtered_messages.append(message)
+            
+            
+     return render_template("chat.html", username=username , messages=filtered_messages)
     
     
     
-@app.route("/<username>/new", methods = ["POST"])
-def get_add_message(username):
+@app.route("/new", methods = ["POST","GET"])
+def get_add_message():
+    
+    username = request.form["username"]
     text = request.form["message"]
 
 
-
-
-    words = text.split()
-    for i in range(len(words)):
-        for j in range(len(banned_words)):
-            if words[i] == banned_words[j]:
-                words[i] = "*"*len(words[i])
-    text = " ".join(map(str,words))
+    words = text.split()  #splits word
+    words = [ "*" * len(word) if word.lower() in banned_words else word for word in words] ##uses a * for each letter of the word if its in banned words
+    text = " ".join(map(str,words)) #rejoins the word back together
+    
+    
     message = { 
     "sender": username,
-    "body": text
+    "body": text,
     }
         
   
